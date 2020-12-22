be6709316a4bc8a81bdf26264dda79838837fe96
Marco Aguilar <marcocaaguilar@gmail.com>
2020-12-21 19:04:05 -0600

    Modificacion envio de parametro perfil

--- Payroll/Controllers/EmpresasController.cs
+++ Payroll/Controllers/EmpresasController.cs
@@ -72,8 +72,7 @@ namespace Payroll.Controllers
         {
             int Perfil_id = int.Parse(Session["Profile"].ToString());
             List<PruebaEmpresaBean> empresas;
-            PruebaEmpresaDao Dao = new PruebaEmpresaDao(); 
-            int Perfil_id = int.Parse(Session["Profile"].ToString()); 
+            PruebaEmpresaDao Dao = new PruebaEmpresaDao();
             empresas = Dao.sp_Retrieve_NombreEmpresas(Perfil_id);
             string btnsEmpresas = "<div class='row'>";
             foreach (var item in empresas)
