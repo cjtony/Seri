91e28ed35aeef73df14fb2b707717612805af604
DELL <DELL@DESKTOP-CNPFA5C>
2021-01-19 09:47:01 -0600

    Actualizacion mensjae de ejecucion de nominas pantalla nomina_ejecucion

--- Payroll/Controllers/NominaController.cs
+++ Payroll/Controllers/NominaController.cs
@@ -320,6 +320,7 @@ namespace Payroll.Controllers
             NominahdBean bean = new NominahdBean();
             FuncionesNomina dao = new FuncionesNomina();
             bean = dao.sp_TpDefinicion_Update_TpDefinicion(sNombreDefinicion, sDescripcion, iAno, iCancelado, iIdDefinicionhd);
+                     
             return Json(bean);
         }
         [HttpPost]
@@ -1184,6 +1185,7 @@ namespace Payroll.Controllers
             FuncionesNomina Dao = new FuncionesNomina();
             LNND = Dao.sp_ExiteDefinicionTpCalculo_Retrieve_ExiteDefinicionTpCalculo(iIdDefinicionHd);         
             return Json(LNND);
+
         }
    
     }
