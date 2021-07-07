ecca7f92d7e3d9b5b4f3bd7380314e377d6bbfe4
MARCO ANTONIO CARRANZA AGUILAR <marcoc@raciti.com.mx>
2021-07-07 10:17:34 -0500

    Correo y eliminacion de ZIP inecesarios

--- Payroll/App_Start/Correo.cs
+++ Payroll/App_Start/Correo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Net.Mail;
+using System.Text;
 
 /// <summary>
 /// Descripción breve de Correo
@@ -82,4 +83,61 @@ public class Correo
     }
 
  
+}
+
+public class MandarCorreos
+{
+    public bool CustomMail(string destinatario, string asunto, string mensaje)
+    {
+        bool status;
+        MailMessage correo = new MailMessage();
+        SmtpClient Protocolo = new SmtpClient();
+        correo.To.Add(destinatario);
+        correo.From = new MailAddress("capitalhumano@gruposeri.com", "Capital Humano", Encoding.UTF8);
+        correo.Subject = asunto;
+        correo.SubjectEncoding = Encoding.UTF8;
+
+        AlternateView htmlView = AlternateView.CreateAlternateViewFromString("<html><body>" + mensaje + "</body></html>", null, System.Net.Mime.MediaTypeNames.Text.Html);
+        correo.BodyEncoding = UTF8Encoding.UTF8;
+        //correo.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+        var aa = AppDomain.CurrentDomain.BaseDirectory;
+
+        LinkedResource imageResource = new LinkedResource(aa + @"Content\img\calendar.png");
+        LinkedResource imageResource2 = new LinkedResource(aa + @"Content\img\calendar.png");
+        LinkedResource imageResource3 = new LinkedResource(aa + @"Content\img\hash.png");
+
+        imageResource.ContentId = "calendar1";
+        imageResource2.ContentId = "calendar2";
+        imageResource3.ContentId = "hashtag";
+        htmlView.LinkedResources.Add(imageResource);
+        htmlView.LinkedResources.Add(imageResource2);
+        htmlView.LinkedResources.Add(imageResource3);
+        correo.AlternateViews.Add(htmlView);
+
+        //correo.Body = mensaje;
+        correo.IsBodyHtml = true;
+        correo.Priority = MailPriority.Normal;
+        correo.BodyEncoding = UTF8Encoding.UTF8;
+
+        Protocolo.Port = 587;
+        Protocolo.Host = "mail.dmlink.com";
+        Protocolo.EnableSsl = true;
+        Protocolo.UseDefaultCredentials = false;
+        //Protocolo.Credentials = new System.Net.NetworkCredential("capitalhumano@gruposeri.com", "cH*150519");
+        Protocolo.Credentials = new System.Net.NetworkCredential("capitalhumano@gruposeri.com", "cH*150519");
+
+        try
+        {
+            Protocolo.Send(correo);
+            status = true;
+        }
+        catch (SmtpException error)
+        {
+            status = false;
+            Console.WriteLine("----------------------------------------------------- ERROR ------------------------------------------------------");
+            Console.WriteLine(error.Message);
+        }
+
+        return status;
+    }
 }
